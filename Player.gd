26f68ec42ce103f720bececd94dc978d90f46f55
trvkv <szymontmcz@gmail.com>
2023-12-18 19:29:44 +0100

    #5: Stupid movement for player added

--- Player.gd
+++ Player.gd
@@ -1,16 +1,15 @@
-extends Sprite3D
+extends CharacterBody3D
 
 var acc: float = 0.0
 var current_frame: int = 0
 
-@export var sprite_frames = 1
+var gravity = 0.9
 
-func _process(delta):
-    acc += delta
-    if acc > 1.0:
-        acc -= 1.0
-        current_frame += 1
-        if current_frame < sprite_frames:
-            frame = current_frame
-        else:
-            current_frame = 0
+@onready var sprite = $AnimatedSprite3D
+
+func _physics_process(_delta):
+    var movement_dir = Vector2(Input.get_axis("ui_down", "ui_up"), Input.get_axis("ui_left", "ui_right"))
+    movement_dir = movement_dir.normalized()
+    #velocity.y -= gravity * delta
+    velocity += Vector3(movement_dir.x, 0.0, movement_dir.y) * 0.12
+    move_and_slide()
