5f4e26e25cad62a858af0ac4427f5bb442ebacde
trvkv <szymontmcz@gmail.com>
2023-12-21 20:51:29 +0100

    #5: Small reorganizing of the scenes and nodes

--- Scenes/Actors/player.gd
+++ Scenes/Actors/player.gd
@@ -7,9 +7,13 @@ var gravity = 0.9
 
 @onready var sprite = $AnimatedSprite3D
 
-func _physics_process(_delta):
+func _physics_process(delta):
+    # gravity comes first
+    velocity.y -= gravity * delta
+
+    # movement
     var movement_dir = Vector2(Input.get_axis("ui_down", "ui_up"), Input.get_axis("ui_left", "ui_right"))
     movement_dir = movement_dir.normalized()
-    #velocity.y -= gravity * delta
     velocity += Vector3(movement_dir.x, 0.0, movement_dir.y) * 0.12
+
     move_and_slide()
