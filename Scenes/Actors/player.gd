e59f9430f036494fa3165a386841aa605df20556
trvkv <szymontmcz@gmail.com>
2023-12-25 21:48:39 +0100

    #14: Simple running and idle animations handled

--- Scenes/Actors/player.gd
+++ Scenes/Actors/player.gd
@@ -3,6 +3,21 @@ extends CharacterBody3D
 @export var movement_speed: float = 4.0
 @export var gravity = 0.9
 
+@onready var animation_player = $AnimationPlayer
+@onready var sprite_position = $SpritesPosition
+
+func _process(_delta):
+    print(velocity)
+    if velocity.length() > 0.0:
+        animation_player.current_animation = "move"
+        if velocity.z > 0.0:
+            sprite_position.rotation.y = deg_to_rad(180.0)
+        if velocity.z < 0.0:
+            sprite_position.rotation.y = deg_to_rad(0.0)
+
+    if velocity == Vector3.ZERO:
+        animation_player.current_animation = "idle"
+
 func _physics_process(delta):
     # gravity comes first
     velocity.y -= gravity * delta
--- camera_position.gd
+++
@@ -1,9 +0,0 @@
-extends Node3D
-
-@export var rotation_speed = 0.3
-
-var rotation_y: float = 0.0
-
-func _process(delta):
-	set_rotation(Vector3(0.0, rotation_y, 0.0))
-	rotation_y += rotation_speed * delta
