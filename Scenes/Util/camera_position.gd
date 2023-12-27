645e33405cb15b22188402b09a0c4bcbbebb984d
trvkv <szymontmcz@gmail.com>
2023-12-27 19:02:17 +0100

    #21: counting of distance to fadeable wall fixed

--- Scenes/Util/camera_position.gd
+++ Scenes/Util/camera_position.gd
@@ -12,8 +12,9 @@ func _process(_delta):
     for body in faded_objects.keys():
         if faded_objects[body]["ray_result"] != null:
             var normal = faded_objects[body]["ray_result"]["normal"]
-            var nearest_wall_position = normal * (position - to_local(body.global_position))
-            set_distance_fade(body, nearest_wall_position.length())
+            var raycast_vec: Vector3 = position - to_local(body.global_position)
+            var nearest_wall_position = cos(raycast_vec.angle_to(normal)) * raycast_vec.length()
+            set_distance_fade(body, nearest_wall_position)
 
 func _physics_process(_delta):
     for body in faded_objects.keys():
@@ -40,4 +41,5 @@ func disable_distance_fade(body: CollisionObject3D):
 func set_distance_fade(body: CollisionObject3D, distance: float):
     var wall: MeshInstance3D = body.get_parent()
     var material: StandardMaterial3D = wall.get_active_material(0)
+    print(distance)
     # ENABLE HERE
