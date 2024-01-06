116ca105d5dbc48d12227e0eca2bd3a6c9c945a4
trvkv <szymontmcz@gmail.com>
2024-01-06 10:16:58 +0100

    #24: [Unit] Added mesh checks for ItemContainer

--- Tests/Unit/test_container.gd
+++ Tests/Unit/test_container.gd
@@ -5,6 +5,19 @@ func test_container_create():
     add_child_autofree(c)
     assert_true(is_instance_valid(c), "ItemContainer created correctly")
 
+func test_mesh_setting():
+    var c1: ItemContainer = load("res://Scenes/Environment/item_container.tscn").instantiate()
+    var s1: PackedScene = load("res://Scenes/Environment/cube_with_collision.tscn")
+    c1.container_mesh_scene = s1
+    add_child_autofree(c1)
+    assert_true(is_instance_valid(c1) and is_instance_valid(c1.container_mesh), "Adding mesh before ready is ok")
+
+    var c2: ItemContainer = load("res://Scenes/Environment/item_container.tscn").instantiate()
+    var s2: PackedScene = load("res://Scenes/Environment/cube_with_collision.tscn")
+    add_child_autofree(c2)
+    c2.container_mesh_scene = s2
+    assert_true(is_instance_valid(c2) and is_instance_valid(c2.container_mesh), "Adding mesh after ready is ok")
+
 func test_negative():
     var c: ItemContainer = load("res://Scenes/Environment/item_container.tscn").instantiate()
     add_child_autofree(c)
@@ -13,11 +26,11 @@ func test_negative():
     assert_false(c.retrieve(k), "Retrieve non existent ok")
 
     assert_true(c.peek().size() == 0, "Peek size is ok")
-    assert_true(c.peek(&"ItemFavourite").size() == 0, "Peek filter is ok")
-    assert_true(c.peek(&"ItemTrap").size() == 0, "Peek filter is ok")
-    assert_true(c.peek(&"ItemKey").size() == 0, "Peek filter is ok")
+    assert_true(c.peek(&"ItemFavourite").size() == 0, "Fav peek filter is ok")
+    assert_true(c.peek(&"ItemTrap").size() == 0, "Trap peek filter is ok")
+    assert_true(c.peek(&"ItemKey").size() == 0, "Key peek filter is ok")
 
-    assert_true(c.peek(&"SomeRandomItem").size() == 0, "Peek filter is ok")
+    assert_true(c.peek(&"SomeRandomItem").size() == 0, "Random peek filter is ok")
 
     k.free()
 
