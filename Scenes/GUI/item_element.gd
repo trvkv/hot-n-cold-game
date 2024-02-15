d0d5aa9e4aa41cf55972bcfb4d1ee89d3cd232a2
trvkv <szymontmcz@gmail.com>
2024-02-15 16:46:20 +0100

    #8: ItemElement script added
    Script is able to set the element icon and number

---
+++ Scenes/GUI/item_element.gd
@@ -0,0 +1,39 @@
+extends PanelContainer
+
+class_name ItemElement
+
+@onready var texture_rect = $MarginContainer/TextureRect
+@onready var number_label = $MarginContainer/MarginContainer/CenterContainer/Panel/LabelNumber
+
+@export var texture: Texture2D = null:
+    set(value):
+        texture = value
+        set_icon(texture)
+    get:
+        return texture
+
+@export var number: int = 0:
+    set(value):
+        number = value
+        set_number(number)
+    get:
+        return number
+
+func _ready():
+    set_number(number)
+    set_icon(texture)
+
+func set_number(value: int) -> void:
+    if not is_instance_valid(number_label):
+        return
+
+    var s = str(value)
+    if not s.is_valid_int():
+        return
+
+    number_label.set_text(s)
+
+func set_icon(icon: Texture2D) -> void:
+    if not is_instance_valid(texture_rect):
+        return
+    texture_rect.texture = icon
