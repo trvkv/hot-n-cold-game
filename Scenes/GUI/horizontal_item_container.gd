70caa01392899a60ee553a3a4603a91fb5b7a98b
trvkv <szymontmcz@gmail.com>
2024-03-09 18:39:18 +0100

    #8: Added simple items to inventory

--- Scenes/GUI/horizontal_item_container.gd
+++ Scenes/GUI/horizontal_item_container.gd
@@ -2,21 +2,31 @@ extends HBoxContainer
 
 class_name HorizontalItemContainer
 
+@export var item_element_scene: PackedScene
 @export var items: Array[ItemBase] = []
 
 func _ready() -> void:
+    assert(is_instance_valid(item_element_scene), "Item element scene is needed")
     reload_items()
 
 func reload_items() -> void:
-    # TODO: not finished yet
     for item_element: ItemElement in get_children():
         # clear current item
         clear_item(item_element)
+    for item in items:
+        add_item(item)
 
-func add_item(item_element: ItemElement, item: ItemBase) -> void:
-    if is_instance_valid(item_element) and is_instance_valid(item):
+func add_item(item: ItemBase) -> void:
+    if not is_instance_valid(item):
+        return
+    var item_element: ItemElement = item_element_scene.instantiate()
+    if is_instance_valid(item_element):
+        add_child(item_element)
         item_element.set_icon(item.icon)
+        item_element.set_number(1)
 
 func clear_item(item_element: ItemElement) -> void:
     if is_instance_valid(item_element):
+        remove_child(item_element)
         item_element.set_icon(null)
+        item_element.queue_free()
