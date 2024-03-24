e9f5da9128555db5d64c3f3089b20e2f841d2757
trvkv <szymontmcz@gmail.com>
2024-03-24 17:32:17 +0100

    #27: actions to strings added + small improvements

--- Scenes/Items/item_actions.gd
+++ Scenes/Items/item_actions.gd
@@ -8,3 +8,15 @@ enum ACTIONS {
     UNLOCK_CONTAINER,
     SET_TRAP
 }
+
+static func action_to_string(action: int) -> String:
+    if action == ACTIONS.PUT_TO_CONTAINER:
+        return "Put to container"
+    elif action == ACTIONS.LOCK_CONTAINER:
+        return "Lock container"
+    elif action == ACTIONS.UNLOCK_CONTAINER:
+        return "Unlock container"
+    elif action == ACTIONS.SET_TRAP:
+        return "Set trap"
+    printerr("Incorrect action ID: %d, returning null" % action)
+    return ""
--- Scenes/Util/interaction_manager.gd
+++ Scenes/Util/interaction_manager.gd
@@ -19,16 +19,25 @@ func _on_update_interactees(player, interactees, active_interactee) -> void:
     if player not in player_interaction_data.keys():
         printerr("FATAL: Player not found in player interaction data!")
         return
+
     player_interaction_data[player]["interactees"] = interactees
     player_interaction_data[player]["active_interactee"] = active_interactee
 
     var gui = get_interaction_gui(player)
-    # resetting
+    if not is_instance_valid(gui):
+        printerr("Interaction gui for %s is invalid" % PlayersManager.get_player_name(player.player_id))
+        return
+
+    # resetting players gui
     for child in gui.get_children():
         gui.remove_child(child)
         child.queue_free()
 
-    # TODO: not finished !!!!!
+    if is_instance_valid(active_interactee):
+        # TODO: for active interactee gather possible actions
+        # and print them as strings in the gui.
+        pass
+
     for interactee in player_interaction_data[player]["interactees"]:
         var label = Label.new()
         label.set_text(str(interactee))
