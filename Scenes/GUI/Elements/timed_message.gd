33820ea64df85057fe48efe25b1a0082362db9e1
trvkv <szymontmcz@gmail.com>
2024-06-20 11:59:41 +0200

    #9: Refreshed TimedMessage added

--- Scenes/GUI/Elements/timed_message.gd
+++ Scenes/GUI/Elements/timed_message.gd
@@ -1,4 +1,4 @@
-extends Label
+extends MarginContainer
 
 class_name TimedMessage
 
@@ -6,11 +6,16 @@ class_name TimedMessage
 @export var timer: Timer
 @export var timeout: float = 5.0
 @export var autostart: bool = false
+@export var text: String = "":
+    set = set_text
+
+@export var message_label: Label
 
 signal dismissing_message(timed_message)
 
 func _ready() -> void:
-    assert(timer, "Timer invalid")
+    assert(is_instance_valid(message_label), "Message label is invalid")
+    assert(is_instance_valid(timer), "Timer invalid")
     timer.connect("timeout", _on_timeout)
     if autostart:
         start()
@@ -18,6 +23,10 @@ func _ready() -> void:
 func start() -> void:
     if timeout > 0:
         timer.start(timeout)
+
+func set_text(message: String) -> void:
+    text = message
+    message_label.text = text
 
 func _on_timeout() -> void:
     emit_signal("dismissing_message", self)
